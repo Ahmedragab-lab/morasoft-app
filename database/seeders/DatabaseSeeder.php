b27eeb69d440f99b3eb4156b2a8e3b1d395d8cdb
wolf <ahmedragabyasin2020@gmail.com>
2021-11-18 07:08:01 +0200

    sections seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call(adminSeeder::class);
         $this->call(ServiceSeeder::class);
+        $this->call(SectionSeeder::class);
         $this->call(FeedbackSeeder::class);
         $this->call(EventSeeder::class);
         $this->call(AgreementSeeder::class);
--- database/seeders/SectionSeeder.php
+++ database/seeders/SectionSeeder.php
@@ -2,17 +2,23 @@
 
 namespace Database\Seeders;
 
+use App\Models\section;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class SectionSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+
     public function run()
     {
-        //
+        DB::table('sections')->delete();
+        $section = new section();
+        $section->section_name = ['en'=>'Mobile','ar'=>'هواتف محموله'];
+        $section->slug = 'mobile';
+        $section->image = 'default.jpg';
+        $section->desc = 'يوجد لدينا منتجات متعدده';
+        $section->status = 1;
+        $section->popular = 1;
+        $section->save();
     }
 }
