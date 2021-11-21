b31b2ade71b959224e493301993befecec52e3e6
wolf <ahmedragabyasin2020@gmail.com>
2021-11-21 20:36:18 +0200

    adjust dashboard log out

--- resources/views/Admin/layouts/footerJs.blade.php
+++ resources/views/Admin/layouts/footerJs.blade.php
@@ -3,7 +3,7 @@
 <script src="{{ URL::asset('admin/en/assets/js/jquery.min.js')}}"></script>
 <script src="{{ URL::asset('admin/en/assets/js/bootstrap.bundle.min.js')}}"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-{{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script> --}}
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
 <script src="{{ URL::asset('admin/en/assets/js/detect.js')}}"></script>
 <script src="{{ URL::asset('admin/en/assets/js/fastclick.js')}}"></script>
 <script src="{{ URL::asset('admin/en/assets/js/jquery.slimscroll.js')}}"></script>
--- resources/views/Admin/layouts/head.blade.php
+++ resources/views/Admin/layouts/head.blade.php
@@ -10,11 +10,11 @@
 <link rel="stylesheet" href="../plugins/morris/morris.css">
 
 @if (App::getLocale() == 'ar')
-     {{-- <link href="{{ URL::asset('admin/ar/assets/css/bootstrap.min.css') }}" rel="stylesheet"> --}}
+     <link href="{{ URL::asset('admin/ar/assets/css/bootstrap.min.css') }}" rel="stylesheet">
      <link href="{{ URL::asset('admin/ar/assets/css/icons.css') }}" rel="stylesheet">
      <link href="{{ URL::asset('admin/ar/assets/css/style.css') }}" rel="stylesheet">
 @else
-    {{-- <link href="{{ URL::asset('admin/en/assets/css/bootstrap.min.css') }}" rel="stylesheet"> --}}
+    <link href="{{ URL::asset('admin/en/assets/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ URL::asset('admin/en/assets/css/icons.css') }}" rel="stylesheet">
     <link href="{{ URL::asset('admin/en/assets/css/style.css') }}" rel="stylesheet">
 @endif
