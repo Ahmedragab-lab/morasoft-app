fcd8dda15c1fe0d83e273f7d800859066089f624
dina-salem-26 <86434887+dina-salem-26@users.noreply.github.com>
2021-12-01 07:09:36 +0300

    controller&noification contact commit

--- app/Models/contact.php
+++ app/Models/contact.php
@@ -5,12 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class contact extends Model
+class Contact extends Model
 {
     use HasFactory;
+
     protected $guarded = [];
 
-    public function users(){
-        return $this->belongsTo(User::class,'user_id');
-    }
 }
