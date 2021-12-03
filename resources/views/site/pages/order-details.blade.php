b538848baa2e59d1381a7803a2239cf66478cf7d
wolf <ahmedragabyasin2020@gmail.com>
2021-12-03 05:44:21 +0200

    show trackin step in order details page

--- resources/views/site/pages/order-details.blade.php
+++ resources/views/site/pages/order-details.blade.php
@@ -36,7 +36,6 @@
                                         </tr>
                                         </thead>
                                         <tbody>
-                                            {{-- @foreach ($order_details as $item) --}}
                                             <tr>
                                                 <td >{{ $order_details->order_no }}</td>
                                                 <td>{{ $order_details->name }}</td>
@@ -45,10 +44,6 @@
                                                 <td>{{ $order_details->to->Name}}</td>
                                                 <td>{{ number_format($order_details->price) }} LE</td>
                                             </tr>
-                                            @php
-                                                // $total_price += $item->price;
-                                            @endphp
-                                            {{-- @endforeach --}}
                                         </tbody>
                                     </table>
                                     <table class="table is-fullwidth is-hoverable">
@@ -96,32 +91,32 @@
       <br>
       <br>
       <div class="steps" data-aos="fade-right">
-        <div class="step-item" data-step-id="0">
+        <div class="step-item {{ $order_step->step==1 ? 'active':'' }}" data-step-id="0">
           <div class="step-marker"> 1 </div>
           <div class="step-details">
-            <p class="step-title">lorem ipsum dolor</p>
-            <p>Lorem ipsum dolor sit amet nulla varius lectus.</p>
+            <p class="step-title">تم تجهيز الشحنه</p>
+            <p>هذه الخطوه يتم تجهيز وسيله النقل وترتيبات النقل.</p>
           </div>
         </div>
-        <div class="step-item active" data-step-id="1">
+        <div class="step-item  {{ $order_step->step==2 ? 'active':'' }}" data-step-id="1">
           <div class="step-marker">2</div>
           <div class="step-details">
-            <p class="step-title">lorem ipsum dolor</p>
-            <p>Lorem ipsum dolor sit amet nulla varius lectus.</p>
+            <p class="step-title">تم التعقيم والتغليف</p>
+            <p>الان يتم عمليه تعقيم وتغليف الشحنه بعنايه لتسليمها اليكم بجوده عاليه.</p>
           </div>
         </div>
-        <div class="step-item" data-step-id="2">
+        <div class="step-item {{ $order_step->step==3 ? 'active':'' }}" data-step-id="2">
           <div class="step-marker">3</div>
           <div class="step-details">
-            <p class="step-title">lorem ipsum dolor</p>
-            <p>Lorem ipsum dolor sit amet nulla varius lectus.</p>
+            <p class="step-title">عمليه المتابعه</p>
+            <p>تم التحرك ومتابعه عمليه سير الشحنه.</p>
           </div>
         </div>
-        <div class="step-item" data-step-id="3">
+        <div class="step-item {{ $order_step->step==4 ? 'active':'' }}" data-step-id="3">
           <div class="step-marker"> 4 </div>
           <div class="step-details">
-            <p class="step-title">lorem ipsum dolor</p>
-            <p>Lorem ipsum dolor sit amet nulla varius lectus.</p>
+            <p class="step-title">التسليم النهائى</p>
+            <p>الان تم استلام الشحنه يرجى تقييمكم للخدمه شكرا لكم مع تحيات مورا سوفت للنقل والشحن.</p>
           </div>
         </div>
       </div>
