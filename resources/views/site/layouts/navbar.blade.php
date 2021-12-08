41e61b54c1e9f3c97ec28221b2cbaf50d9dcc7dd
AsmaaHosnyAly <49831110+AsmaaHosnyAly@users.noreply.github.com>
2021-12-08 12:18:54 +0200

    Navbar done

--- resources/views/site/layouts/navbar.blade.php
+++ resources/views/site/layouts/navbar.blade.php
@@ -5,14 +5,14 @@
                 <div class="level-left">
                     <div id="header-logo" class="site-logo ">
                         <div id="logo-inner" class="site-logo-inner">
-                            <a href="{{ route('front.index') }}"  class=" nav-link active">
-                                 {{-- <img alt="Joo - Niche Multi-Purpose HTML Template" --}}
-                                     {{-- src="{{ asset('assets/images/logo.png') }}" width="200" height="200">  --}}
-                              {{-- <span> <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="truck-moving" class="svg-inline--fa fa-truck-moving fa-w-1 text-dark"
+                            <a href="{{ route('front.index') }}" class=" nav-link active">
+                                {{-- <img alt="Joo - Niche Multi-Purpose HTML Template" --}}
+                                {{-- src="{{ asset('assets/images/logo.png') }}" width="200" height="200"> --}}
+                                {{-- <span> <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="truck-moving" class="svg-inline--fa fa-truck-moving fa-w-1 text-dark"
                                role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path
                                 fill="currentColor" d="M621.3 237.3l-58.5-58.5c-12-12-28.3-18.7-45.3-18.7H480V64c0-17.7-14.3-32-32-32H32C14.3 32 0 46.3 0 64v336c0 44.2 35.8 80 80 80 26.3 0 49.4-12.9 64-32.4 14.6 19.6 37.7 32.4 64 32.4 44.2 0 80-35.8 80-80 0-5.5-.6-10.8-1.6-16h163.2c-1.1 5.2-1.6 10.5-1.6 16 0 44.2 35.8 80 80 80s80-35.8 80-80c0-5.5-.6-10.8-1.6-16H624c8.8 0 16-7.2 16-16v-85.5c0-17-6.7-33.2-18.7-45.2zM80 432c-17.6 0-32-14.4-32-32s14.4-32 32-32 32 14.4 32 32-14.4 32-32 32zm128 0c-17.6 0-32-14.4-32-32s14.4-32 32-32 32 14.4 32 32-14.4 32-32 32zm272-224h37.5c4.3 0 8.3 1.7 11.3 4.7l43.3 43.3H480v-48zm48 224c-17.6 0-32-14.4-32-32s14.4-32 32-32 32 14.4 32 32-14.4 32-32 32z"></path></svg></span> --}}
                                 {{-- <i class="fas fa-truck-moving text-dark logo-text "></i> --}}
-                                    <span class="logo-text">{{ __('front.mora') }}</span>
+                                <span class="logo-text">{{ __('front.mora') }}</span>
                             </a>
                         </div>
                         <!-- #logo-inner -->
@@ -25,68 +25,85 @@
                         <nav class="main-navigation right">
                             <ul class="menu">
                                 <li>
-                                    <a  href="{{ route('front.index') }}" class="nav-link active"><i class="fas fa-home"></i></a>
+                                    <a href="{{ route('front.index') }}" class="nav-link active"><i
+                                            class="fas fa-home"></i></a>
                                 </li>
-                               
+
                                 <li class="has-dropdown">
-                                    <a href="#"  class="nav-link"> <i class="fas fa-handshake"></i>  {{ __('front.services') }}</a>
+                                    <a href="#" class="nav-link"> <i class="fas fa-handshake"></i>
+                                        {{ __('front.services') }}</a>
 
                                     <ul class="dropdown">
-                                        @foreach ( \App\Models\Service::where('status','1')->get() as $service)
-                                           <li><a href="{{ route('allservices.show',$service->id) }}"> <i class="fas fa-helicopter"></i> {{ $service->serve_name }}</a></li>
-                                           <li></li>
+                                        @foreach (\App\Models\Service::where('status', '1')->get() as $service)
+                                            <li><a href="{{ route('allservices.show', $service->id) }}"> <i
+                                                        class="fas fa-helicopter"></i> {{ $service->serve_name }}</a>
+                                            </li>
+                                            <li></li>
                                         @endforeach
                                     </ul>
                                 </li>
                                 <li class="has-dropdown">
                                     <a href="#"> {{ __('front.sections') }}</a>
                                     <ul class="dropdown">
-                                        @foreach (\App\Models\Section::where('status','1')->get() as $section)
-                                        <li><a href="{{ route('allsections.show',$section->id) }}">{{$section->section_name}}</a></li>
+                                        @foreach (\App\Models\Section::where('status', '1')->get() as $section)
+                                            <li><a
+                                                    href="{{ route('allsections.show', $section->id) }}">{{ $section->section_name }}</a>
+                                            </li>
                                         @endforeach
                                     </ul>
                                 </li>
                                 <li>
-                                    <a href="{{ route('allproducts.index')}}"  class="nav-link">{{ __('front.products') }}</a>
+                                    <a href="{{ route('allproducts.index') }}"
+                                        class="nav-link">{{ __('front.products') }}</a>
                                 </li>
                                 <li>
-                                    <a href="{{ route('allfeedback.index')}}"  class="nav-link">{{ __('front.feedback') }} </a>
+                                    <a href="{{ route('allfeedback.index') }}"
+                                        class="nav-link">{{ __('front.feedback') }} </a>
                                 </li>
                                 @auth
-                                <li>
-                                    <a href="{{ route('contact.index')}}"  class="nav-link">{{ __('front.contactus') }}</a>
-                                </li>
+                                    <li>
+                                        <a href="{{ route('contact.index') }}"
+                                            class="nav-link">{{ __('front.contactus') }}</a>
+                                    </li>
                                 @endauth
                                 <li class="has-dropdown">
                                     <a href="#">Order Now</a>
                                     <ul class="dropdown">
                                         <li>
-                                            <a href="#order" >
+                                            <a href="#order">
                                                 <i class="fas fa-edit"></i> Make an order
                                             </a>
                                         </li>
-                                        <li><a href="#tracking"><i class="fas fa-map-marker-alt"></i>  Tracking an order </a></li>
+                                        <li><a href="#tracking"><i class="fas fa-map-marker-alt"></i> Tracking an order
+                                            </a></li>
                                     </ul>
                                 </li>
                                 @guest
-                                <li class="btn-cta"><a href="/login"  class="nav-link"><span>Login</span></a></li>
-                                <li class="btn-cta"><a href="/register"  class="nav-link"><span>register</span></a></li>
+                                    <li class="btn-cta"><a href="/login"
+                                            class="nav-link"><span>Login</span></a></li>
+                                    <li class="btn-cta"><a href="/register"
+                                            class="nav-link"><span>register</span></a></li>
                                 @endguest
                                 @auth
                                     <li class="has-dropdown">
-                                        <a href="#"  class="nav-link"><img src={{ asset('uploads/user-img/'.Auth::user()->image) }} width="30" class="rounded-circle" >
-                                             {{ \Str::limit(auth()->user()->fname, 10) }}
+                                        <a href="#" class="nav-link"><img
+                                                src={{ asset('uploads/user-img/' . Auth::user()->image) }} width="30"
+                                                class="rounded-circle">
+                                            {{ \Str::limit(auth()->user()->fname, 10) }}
                                         </a>
                                         <ul class="dropdown">
-                                            <li><a href="{{ route('userprofile.show',Auth::user()->id)}}"><i class=" far fa-address-card"></i> My Profile</a></li>
+                                            <li><a href="{{ route('userprofile.show', Auth::user()->id) }}"><i
+                                                        class=" far fa-address-card"></i> My Profile</a></li>
                                             {{-- <li><a href="#"><i class=" far fa-comment-dots"></i> my orders</a></li> --}}
-                                            <li><a href="{{ route('mycart.index')}}"><i class="icon-basket"></i> my cart</a></li>
+                                            <li><a href="{{ route('mycart.index') }}"><i class="icon-basket"></i> my
+                                                    cart</a></li>
                                             <li>
-                                                <a   class="nav-link" href="{{ route('logout') }}"
+                                                <a class="nav-link" href="{{ route('logout') }}"
                                                     onclick="event.preventDefault();document.getElementById('logout-form').submit();">
                                                     <i class="far fa-trash-alt"></i> log out
                                                 </a>
-                                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                                    style="display: none;">
                                                     @csrf
                                                 </form>
                                             </li>
@@ -98,24 +115,32 @@
                                                 {{ auth()->user()->unreadNotifications->count() }}
                                             </span>
                                         </a>
-                                        <ul class="dropdown">
+                                        <ul class="dropdown" style="width: 300px">
                                             <div id="unread">
-                                                @foreach(auth()->user()->unreadNotifications as $notification)
-                                                    <li>
-                                                        <a href="{{ route('order_details.show',$notification->data['id']) }}">
+                                                @foreach (auth()->user()->unreadNotifications as $notification)
+                                                    <li style="text-align: left">
+                                                        <a href="{{ route('order_details.show', $notification->data['id']) }}"
+                                                            style="text-align: left">
                                                             <h4 style="color:rgb(0, 0, 0); font-size:15px;">
+                                                                <p style="font-weight:bold;font-size:20px"> <img
+                                                                        src={{ asset('assets/images/logo1.png') }}
+                                                                        width="40"
+                                                                        class="rounded-circle  border-3  border border-dark me-3">
+                                                                    Morasfot </p><p style="font-size: 15px; color:dark" >Welcome
+                                                                    {{ $notification->data['user'] }}</p>
+
                                                                 {{ $notification->data['title'] }}
-                                                                <span style="color: rgb(226, 33, 33); font-size:20px; font-weight:bold;">
-                                                                    {{ (number_format($notification->data['price'] ,2)) }} LE
+                                                                <span
+                                                                    style="color: rgb(226, 33, 33); font-size:15px; font-weight:bold;">
+                                                                    {{ number_format($notification->data['price'], 2) }}
+                                                                    LE
                                                                 </span>
-                                                                of order number
-                                                                <span style="color: rgb(33, 117, 226); font-size:15px;">
+                                                               order number is :
+                                                                <span style="background-color: rgb(202, 206, 212); font-size:15px;">
                                                                     {{ $notification->data['order_no'] }}
                                                                 </span>
-                                                                thank you
-                                                                <span style="color: rgb(33, 123, 226); font-size:15px;">
-                                                                {{ $notification->data['user'] }}
-                                                                </span>
+                                                                for more details click on this message.
+                                                                
                                                             </h4>
                                                         </a>
                                                     </li>
@@ -129,14 +154,17 @@
                                             <span class="icon ">
                                                 <i class="icon-bell"></i>
                                                 <span class="badge notif-count"
-                                                 data-count="{{ \App\Models\Usernoti::count() }}" >{{ \App\Models\Usernoti::count() }}
+                                                    data-count="{{ \App\Models\Usernoti::count() }}">{{ \App\Models\Usernoti::count() }}
                                                 </span>
                                             </span>
                                         </a>
-                                        <ul class="dropdown-notification scrollable-container" >
+                                        <ul class="dropdown-notification scrollable-container">
 
-                                            <li class="header">notifications(<span class="notif-count">{{ \App\Models\Usernoti::count() }}</span>)</li>
-                                            @foreach (\App\Models\Usernoti::orderByDesc('id')->take(5)->get() as $noti)
+                                            <li class="header">notifications(<span
+                                                    class="notif-count">{{ \App\Models\Usernoti::count() }}</span>)
+                                            </li>
+                                            @foreach (\App\Models\Usernoti::orderByDesc('id')->take(5)->get()
+        as $noti)
                                                 <li>
                                                     <a href="#">
                                                         <strong>{{ $noti->price }}</strong>
