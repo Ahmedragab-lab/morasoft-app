b2ac953b6fb89f4fbc0655ad3069f78407b47f9d
wolf <ahmedragabyasin2020@gmail.com>
2021-12-09 10:32:02 +0200

    check out validation and cart empty fix

--- app/Http/Controllers/Frontend/CheckoutController.php
+++ app/Http/Controllers/Frontend/CheckoutController.php
@@ -11,6 +11,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class CheckoutController extends Controller
 {
@@ -45,10 +46,38 @@ class CheckoutController extends Controller
             // if(Order::whereNotIn('order_id',$ids )){
                 // $order = Order::whereNotIn('order_id',$ids )->get();
                 // $order = Order::where('order_id',Auth::id() )->exists();
-            $exist = Order::where('order_id',Auth::id())->first();
+            $no=Order::select('tracking_no')->get();
+            // dd($no);
+            $exist = Order::whereNotIn('tracking_no',$no)->first();
             if($exist){
-                return redirect()->back()->with('status',"you are already order bafore");
-            }else{
+                return redirect()->back()->with('status',"cart is empty");
+            }else
+            {
+                $validator = Validator::make($request->all(),
+                [
+                    'phone'    => 'required|min:11|numeric',
+                    'address1' => 'required|min:3|max:20|regex:/^([a-zA-Z]+)(\s[a-zA-Z]+)*$/',
+                    'address2' => 'required|min:3|max:20|regex:/^([a-zA-Z]+)(\s[a-zA-Z]+)*$/',
+                    'city'     => 'required|min:3|max:20|regex:/^([a-zA-Z]+)(\s[a-zA-Z]+)*$/',
+                    'country'  => 'required|min:3|max:20|regex:/^([a-zA-Z]+)(\s[a-zA-Z]+)*$/',
+                ]);
+                if ($validator->fails())
+                {
+                    // return response()->json(['status'=>$validator->errors()->first()]);
+                    return redirect()->back()->with('status',$validator->errors()->first());
+                }
+                // valiadtion
+                if(Auth::user()->address1 == NULL){
+                    $user = User::where('id',Auth::id())->first();
+                    $user->fname = $request->fname;
+                    $user->lname = $request->lname;
+                    $user->phone = $request->phone;
+                    $user->address1 = $request->address1;
+                    $user->address2 = $request->address2;
+                    $user->city = $request->city;
+                    $user->country = $request->country;
+                    $user->update();
+                }
                 $order = new Order();
                 $order->order_id = Auth::id();
                 $order->fname = $request->fname;
@@ -61,7 +90,6 @@ class CheckoutController extends Controller
                 $order->country = $request->country;
                 $order->tracking_no = 'morasoft'.rand(1000000000, 9999999999);
                 $order->save();
-            }
                 $cartitems= Cart::where('user_id',Auth::id())->get();
                 foreach($cartitems as $item){
                     OrderItem::create([
@@ -77,21 +105,13 @@ class CheckoutController extends Controller
                     $product->qty = $product->qty - $item->product_qty;
                     $product->update();
                 }
-                if(Auth::user()->address1 == NULL){
-                    $user = User::where('id',Auth::id())->first();
-                    $user->fname = $request->fname;
-                    $user->lname = $request->lname;
-                    $user->phone = $request->phone;
-                    $user->address1 = $request->address1;
-                    $user->address2 = $request->address2;
-                    $user->city = $request->city;
-                    $user->country = $request->country;
-                    $user->update();
-                }
+
                 $cartitems= Cart::where('user_id',Auth::id())->get();
                 Cart::destroy($cartitems);
                 DB::commit();
-                return redirect()->back()->with('status',"your order done");
+                return redirect()->route('mycart.index')->with('status',"your order done");
+                // route('mycart.index')
+            }
 
         } catch (\Exception $e) {
             DB::rollback();
