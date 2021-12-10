4b4a646be3d2d96d9c924e50c364ae70b9dcab74
AsmaaHosnyAly <49831110+AsmaaHosnyAly@users.noreply.github.com>
2021-12-10 19:16:08 +0200

    events page done

--- resources/views/site/pages/last_Event.blade.php
+++ resources/views/site/pages/last_Event.blade.php
@@ -1,118 +1,96 @@
-
-
 @extends('site.layouts.master')
 @section('title') {{ $Last_event->event_title }} @endsection
 @section('css')
-   <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 @endsection
 @section('content')
-<div id="content-main-wrap" class="is-clearfix">
-    <div id="content-area" class="site-content-area">
-      <div id="content-area-inner" class="site-content-area-inner">
-        <section class="section works-list portfolio-single product-single is-clearfix">
-          <div class="container">
-            <br>
-            <div class="columns is-multiline is-variable is-6 product_data">
-              <div class="column is-6">
-                <div class="flexslider thumbnails">
-                  <ul class="slides">
+    <div id="content-main-wrap" class="is-clearfix">
+        <div id="content-area" class="site-content-area">
+            <div id="content-area-inner" class="site-content-area-inner">
+                <section class="section works-list portfolio-single product-single is-clearfix">
+                  <p class="heading-title-top has-text-centered pt-5">welcome logistics</p>
+                  <h1 class="heading-title style-3"> Last
+                      {{-- <br> difference. --}}
+                      <span class="has-text-primary">event </span>
+                  </h1>
+                    <div class="fluid-container">
+                        <div id="content-main-wrap" class="is-clearfix  m-2 p-5">
+                            <div id="content-area" class="site-content-area">
+                                <div id="content-area-inner" class="site-content-area-inner">
+                                    <section
+                                    
+                                        class="section hero has-background-primary-light padding-bottom-none padding-top-none is-clearfix">
+                                        <div>
+                                          
+                                            <div class="columns is-multiline is-variable is-8">
+                                                <div class="column is-6-desktop is-12-tablet">
+                                                    <figure>
+                                                        <img alt="Joo - Niche Multi-Purpose HTML Template"
+                                                            src="{{ asset('uploads/event/' . $Last_event->image) }}"
+                                                            class="is-block" style="height: 450px">
+                                                    </figure>
+                                                </div>
+                                                <div class="column is-6-desktop is-12-tablet pt-5" data-aos="fade-up">
+                                                    <div>
+                                                        <h1 class="heading-title style-1 has-text-left">
+                                                            {{ $Last_event->event_title }}</h1>
+                                                        <p class="heading-title-bottom has-text-center"
+                                                            style="line-height: 1.9;">
+                                                            {{ $Last_event->desc }}
 
-                </div>
-              </div>
-                <div class="column is-6">
-                    <img alt="Joo - Niche Multi-Purpose HTML Template" src="{{ asset('uploads/event/'. $Last_event->image) }}"> </a>
-                <h1 class="heading-title style-1 has-text-left">{{ $Last_event->event_title }}</h1>
-
-                <!-- <span class="icon">
-
-      </span> -->
-    </div>
-    <!-- <br>
-
-    <br> -->
-    <p> {{ $Last_event->desc }}.</p>
-    <br>
-
-
-</div>
-</div>
-
-
-                  {{-- ****************  Start View All *********************** --}}
-
-
-
-
-
-
-                  <table class="table table-striped mb-0">
-                  <tbody>
-                    @foreach ( $events as $index=>$event )
-                    <tr>
-                                {{-- ******************************************* --}}
-                                {{-- <th scope="row">{{ $index +1 }}</th> --}}
-                                <div class="tab-block">
-                                    <br>
-                                    <ul class="comment-list comments comments-box ">
-                                <li class="media comment comment-box">
-                                    <figure class="media-left avatar">
-                                      <p class="image">
-                                        <img alt="Joo - Niche Multi-Purpose HTML Template" src="{{ asset('uploads/event/'.$event->image) }}"> </p>
-                                    </figure>
-                                    <!-- .avatar -->
-                                    <div class="media-content comment-body">
-                                      <div class="level">
-                                        <div class="level-left">
-                                          <span class="comment-author">
-                                            <a href="#">{{ $event->event_title }}</a>
-                                          </span>
-                                          <span class="comment-date">
-                                            <a href="#">APRIL 5, 2018</a>
-                                          </span>
-                                         </div>
-                                            <div class="level-right">
-                                            {{-- <div class="shop-rating">
-                                                <span class="icon">
-                                                <i class="ion-md-star"></i>
-                                                </span>
-                                                <span class="icon">
-                                                <i class="ion-md-star"></i>
-                                                </span>
-                                                <span class="icon">
-                                                <i class="ion-md-star"></i>
-                                                </span>
-                                                <span class="icon">
-                                                <i class="ion-md-star"></i>
-                                                </span>
-                                                <span class="icon">
-                                                <i class="ion-md-star"></i>
-                                                </span>
-                                            </div> --}}
+                                                        </p>
+                                                        <a href="#order" class=" btn  btn-order  rounded-pill "> Order Now
+                                                        </a>
+                                                    </div>
+                                                </div>
                                             </div>
-                                         </div>
-                                      <div class="content">
-                                        <p>{{ $event->desc }}.</p>
-                                      </div>
+                                        </div>
+                                    </section>
+                                </div>
+                            </div>
+                        </div>
+                        </span>
+                    </div>
+                </section>
+
+                {{-- ****************  Start View All *********************** --}}
+
+
+                {{-- Start Asmaa section service --}}
+                <section id="services" class="pt-5">
+                    <div class="container">
+                        <p class="heading-title-top has-text-centered pt-5">welcome logistics</p>
+                        <h1 class="heading-title style-3"> Our
+                            {{-- <br> difference. --}}
+                            <span class="has-text-primary">Events </span>
+                        </h1>
+                        <div class="slider">
+                            <div id="slider-service" class="owl-carousel">
+                                @foreach (\App\Models\Event::orderBy('id', 'DESC')->limit(3)->get()
+        as $event)
+                                    <div class="slider-card">
+                                        <div class="d-flex justify-content-center align-items-center mb-4"
+                                            style="max-height: 350px ;">
+                                            <a href="{{ route('allservices.show', $event->id) }}">
+                                                <img src="{{ asset('uploads/event/' . $event->image) }}"
+                                                    style="height: 300px ;border-radius:25px">
+                                            </a>
+                                        </div>
+                                        <h2 class="mb-0 text-center"><b>{{ $event->event_title }} Event</b></h2>
                                     </div>
-                                    <!-- .comment-body -->
-                                  </li>
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
+                </section>
 
 
-
-{{-- *************************************************** --}}
-                        {{-- <th scope="row">{{ $index +1 }}</th>
-                        <td>{{ $event->event_title }}</td>
-                        <td>{{ $event->limit }}</td>
-                        <td>{{ $event->desc }}</td>
-                        <td>
-                            <img src="{{ asset('uploads/event/'.$event->image) }}" class="img-thumbnail" width="70" alt="">
-                        </td> --}}
+                {{-- End Asmaa  section service --}}
+            </div>
+        </div>
+    </div>
 
 
-
-                    @endforeach
-                </tbody>
-            </table>
 
 
 
